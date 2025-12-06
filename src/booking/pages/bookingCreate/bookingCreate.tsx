fc2bfdef8c6ec50c139df22f4ffd6e4e9765e861
Christian-GL <christiangl202@hotmail.com>
2025-12-06 20:10:00 +0100

    bookingCreate actualizado

--- src/booking/pages/bookingCreate/bookingCreate.tsx
+++ src/booking/pages/bookingCreate/bookingCreate.tsx
@@ -10,11 +10,13 @@ import { ToastifySuccess } from "../../../common/components/toastify/successPopu
 import { ToastifyError } from "../../../common/components/toastify/errorPopup/toastifyError"
 import { AppDispatch } from "../../../common/redux/store"
 import { ApiStatus } from "../../../common/enums/ApiStatus"
+import { OptionYesNo } from "../../../common/enums/optionYesNo"
 import { BookingInterfaceNoId } from "../../interfaces/bookingInterface"
+import { createFormHandlers } from '../../../common/utils/formHandlers'
 import {
-    validatePhoto, validateFullName, validateCheckInCheckOut,
-    validateDateIsOccupied, validateTextArea
-} from '../../../common/utils/validators'
+    validatePhoto, validateFullName, validateEmail, validatePhoneNumber, validateDateRelativeToAnother,
+    validateTextArea, validateRole, validateNewPassword, validateOptionYesNo
+} from '../../../common/utils/commonValidator'
 import {
     GlobalDateTimeStyles, DivCtnForm, DivIcon, DivCtnIcons, IconCalendar, IconPlus, TitleForm, Form, InputTextPhoto, ImgUser, DivCtnEntry,
     LabelText, LabelTextNote, InputText, TextAreaJobDescription, Select, Option, InputDate, DivButtonCreateUser
@@ -36,15 +38,22 @@ export const BookingCreate = () => {
     const roomAll = useSelector(getRoomAllData)
     const roomAllLoading = useSelector(getRoomAllStatus)
     const [newBooking, setNewBooking] = useState<BookingInterfaceNoId>({
-        photo: '',
-        full_name_guest: '',
-        order_date: '',
-        check_in_date: '',
-        check_out_date: '',
+        order_date: new Date(),
+        check_in_date: new Date(),
+        check_out_date: new Date(),
+        price: 0,
         special_request: '',
-        room_id: 0
+        isArchived: OptionYesNo.yes,
+        room_id_list: [],
+        client_id: ''
     })
-    const roomsAvailable = roomAll.filter(room => !validateDateIsOccupied(newBooking, bookingAll.filter(booking => booking.room_data._id === room._id)).length)
+    const { handleStringChange,
+        handleDateChange,
+        handlePhotoChange,
+        handleTextAreaChange,
+        handleSelectChange
+    } = createFormHandlers(setNewBooking)
+    // const roomsAvailable = roomAll.filter(room => !validateDateIsOccupied(newBooking, bookingAll.filter(booking => booking.room_data._id === room._id)).length)
 
     useEffect(() => {
         if (bookingAllLoading === ApiStatus.idle) { dispatch(BookingFetchAllThunk()) }
@@ -57,58 +66,24 @@ export const BookingCreate = () => {
         else if (roomAllLoading === ApiStatus.rejected) { alert("Error in API create booking > rooms") }
     }, [roomAllLoading, roomAll])
 
+    const validateAllData = (): string[] => {
+        const allErrorMessages: string[] = []
 
-    const handlePhotoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, files } = e.target
-        if (files && files[0]) {
-            const photoUrl = URL.createObjectURL(files[0])
-            setNewBooking({
-                ...newBooking,
-                [name]: photoUrl
-            })
-        }
-    }
-    const handleStringInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target
-        setNewBooking({
-            ...newBooking,
-            [name]: value
-        })
-    }
-    const handleNumberSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const { name, value } = e.target
-        setNewBooking({
-            ...newBooking,
-            [name]: parseInt(value)
-        })
-    }
-    const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target
+        // !!! IMPORTAR VALIDADORES DE LA API
 
-        if (!value) return
-        const date = new Date(value)
-        const dateISO = date.toISOString()
-
-        setNewBooking({
-            ...newBooking,
-            [name]: dateISO
-        })
-    }
-    const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        const { name, value } = e.target
-        setNewBooking({
-            ...newBooking,
-            [name]: value
-        })
+        return allErrorMessages
     }
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        if (!validateAllData()) { return }
+        if (validateAllData().length > 0) {
+            validateAllData().forEach(error => ToastifyError(error))
+            return
+        }
 
         const newBookingToDispatch = {
             ...newBooking,
-            order_date: new Date().toISOString()
+            order_date: new Date()
         }
 
         dispatch(BookingCreateThunk(newBookingToDispatch))
@@ -120,28 +95,6 @@ export const BookingCreate = () => {
             .catch((error) => {
                 ToastifyError(error)
             })
-    }
-
-    const validateAllData = (): boolean => {
-        // const errorsPhoto = validatePhoto(newBooking.photo, 'Photo')
-        // if (errorsPhoto.length > 0) { errorsPhoto.map(error => ToastifyError(error)); return false }
-
-        const errorsFullName = validateFullName(newBooking.full_name_guest, 'Full name guest')
-        if (errorsFullName.length > 0) { errorsFullName.map(error => ToastifyError(error)); return false }
-
-        const errorsCheckInDate = validateCheckInCheckOut(new Date(newBooking.check_in_date), new Date(newBooking.check_out_date))
-        if (errorsCheckInDate.length > 0) { errorsCheckInDate.map(error => ToastifyError(error)); return false }
-
-        const errorsSpecialRequest = validateTextArea(newBooking.special_request, 'Special request')
-        if (errorsSpecialRequest.length > 0) { errorsSpecialRequest.map(error => ToastifyError(error)); return false }
-
-        const roomOfBooking = roomAll.find(room => room._id === newBooking.room_id)
-        const bookingsOfRoom = bookingAll.filter(booking => booking.room_data.number === roomOfBooking?.number)
-
-        const errorsDateIsOccupied = validateDateIsOccupied(newBooking, bookingsOfRoom)
-        if (errorsDateIsOccupied.length > 0) { errorsDateIsOccupied.map(error => ToastifyError(error)); return false }
-
-        return true
     }
 
 
@@ -162,17 +115,6 @@ export const BookingCreate = () => {
 
                 <Form onSubmit={handleSubmit}>
                     <DivCtnEntry>
-                        <LabelText>Guest photo</LabelText>
-                        <InputTextPhoto name="photo" type='file' onChange={handlePhotoChange} />
-                        <ImgUser src={newBooking.photo} />
-                    </DivCtnEntry>
-
-                    <DivCtnEntry>
-                        <LabelText>Full name guest</LabelText>
-                        <InputText name="full_name_guest" onChange={handleStringInputChange} />
-                    </DivCtnEntry>
-
-                    <DivCtnEntry>
                         <LabelText>Check in date</LabelText>
                         <InputDate name="check_in_date" type="datetime-local" onChange={handleDateChange} />
 
@@ -182,7 +124,8 @@ export const BookingCreate = () => {
 
                     <DivCtnEntry>
                         <LabelText>Room number</LabelText>
-                        <Select
+                        {/* !!! ACTUALIZAR AL TENER LOS DATOS: */}
+                        {/* <Select
                             name="room_id"
                             onChange={handleNumberSelectChange}
                             disabled={!newBooking.check_in_date || !newBooking.check_out_date || roomsAvailable.length === 0}
@@ -203,7 +146,7 @@ export const BookingCreate = () => {
                                         </>)
                                     }
                                 </>)}
-                        </Select>
+                        </Select> */}
                     </DivCtnEntry>
 
                     <DivCtnEntry>
